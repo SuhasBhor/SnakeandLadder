5341edf1b7a7f3e78b432fa2a1cfe5a238205b20
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2021-11-05 13:48:55 +0530

    Dice Rolled

--- src/com/snakeLadder/snakeLadder.java
+++ src/com/snakeLadder/snakeLadder.java
@@ -1,9 +1,15 @@
 package com.snakeLadder;
 
+import java.util.Random;
+
 public class snakeLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Snake and Ladder Game");
 		int position=0;
-		System.out.println("Player Postion is at " +position);
+		Random rand = new Random();
+		int dicevalue=rand.nextInt(6)+1;
+		System.out.println("Player Position is at " +position);
+		System.out.println("Dice Value is " +dicevalue);
+		System.out.println("Player should move " +dicevalue +" place");
 	}
 }
