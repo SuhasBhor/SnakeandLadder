4281c3c2e0d6c8cf57591f11b4d9002f47acf7b2
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2021-11-05 14:00:25 +0530

    Check Option

--- src/com/snakeLadder/snakeLadder.java
+++ src/com/snakeLadder/snakeLadder.java
@@ -5,11 +5,23 @@ import java.util.Random;
 public class snakeLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Snake and Ladder Game");
-		int position=0;
+		int position=6;
 		Random rand = new Random();
 		int dicevalue=rand.nextInt(6)+1;
-		System.out.println("Player Position is at " +position);
+		System.out.println("Player Initial Position is at " +position);
 		System.out.println("Dice Value is " +dicevalue);
-		System.out.println("Player should move " +dicevalue +" place");
+		
+		int option = rand.nextInt(3);
+		if (option == 0) {
+			System.out.println("No Play. Stay in Same Place");
+		}else if(option == 1){
+			System.out.println("Ladder");
+			position += dicevalue;
+			System.out.println("Current Position is " +position);
+		}else {
+			System.out.println("Snake");
+			position -= dicevalue;
+			System.out.println("Current Position is " +position);
+		}
 	}
 }
