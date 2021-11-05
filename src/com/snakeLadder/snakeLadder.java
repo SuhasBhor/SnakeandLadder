b90d937839715e5ff6a4a0e8fcb9621a22ec62cd
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2021-11-05 14:21:08 +0530

    Winning Position

--- src/com/snakeLadder/snakeLadder.java
+++ src/com/snakeLadder/snakeLadder.java
@@ -4,24 +4,32 @@ import java.util.Random;
 
 public class snakeLadder {
 	public static void main(String[] args) {
-		System.out.println("Welcome To Snake and Ladder Game");
-		int position=6;
-		Random rand = new Random();
-		int dicevalue=rand.nextInt(6)+1;
-		System.out.println("Player Initial Position is at " +position);
-		System.out.println("Dice Value is " +dicevalue);
-		
-		int option = rand.nextInt(3);
-		if (option == 0) {
-			System.out.println("No Play. Stay in Same Place");
-		}else if(option == 1){
-			System.out.println("Ladder");
-			position += dicevalue;
-			System.out.println("Current Position is " +position);
-		}else {
-			System.out.println("Snake");
-			position -= dicevalue;
-			System.out.println("Current Position is " +position);
-		}
-	}
+		int position = 0;  //initial position
+		final int WINNING_POSITION = 100;  //winning position
+
+        //using while loop
+		while (position <= WINNING_POSITION) {
+			Random rand = new Random(); //random function for taking random numbers
+            int diceValue = rand.nextInt(6) + 1; //it gives random numbers between 1 to 6
+            System.out.println("Dice Value is ==> " + diceValue);
+            int option = rand.nextInt(3); //for taking 3 random numbers for taking option
+
+            if (option == 0) {
+            	System.out.println("No Play");
+            }else if (option == 1) {
+                System.out.println("Ladder");
+                position += diceValue; //add dice value in position 
+                System.out.println("Current Position is ==>" + position);
+            }else if (option == 2) {
+                System.out.println("Snake");
+                position -= diceValue;  //sub dice value from position
+                System.out.println("Current Position is ==>" + position);
+
+                if (position < 0) {
+                    position = 0;
+                    System.out.println("Current Position is ==>" + position);
+                }
+            }
+        }
+    }
 }
