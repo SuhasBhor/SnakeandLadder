e37e6e83014b4a17abefc19dc65a49349a0890ea
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2021-11-05 13:41:10 +0530

    UC-1

---
+++ src/com/snakeLadder/snakeLadder.java
@@ -0,0 +1,9 @@
+package com.snakeLadder;
+
+public class snakeLadder {
+	public static void main(String[] args) {
+		System.out.println("Welcome To Snake and Ladder Game");
+		int position=0;
+		System.out.println("Player Postion is at " +position);
+	}
+}
