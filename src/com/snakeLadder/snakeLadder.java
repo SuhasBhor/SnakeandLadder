1e61bede28b0be92dd4ec069cac11b0c6f1ce0c0
SuhasBhor <80667151+SuhasBhor@users.noreply.github.com>
2021-11-05 14:40:23 +0530

    Exact Winning Position

--- src/com/snakeLadder/snakeLadder.java
+++ src/com/snakeLadder/snakeLadder.java
@@ -5,31 +5,42 @@ import java.util.Random;
 public class snakeLadder {
 	public static void main(String[] args) {
 		int position = 0;  //initial position
-		final int WINNING_POSITION = 100;  //winning position
+		final int winningPosition = 100;  //winning position
 
         //using while loop
-		while (position <= WINNING_POSITION) {
+		while (position <= winningPosition) {
 			Random rand = new Random(); //random function for taking random numbers
-            int diceValue = rand.nextInt(6) + 1; //it gives random numbers between 1 to 6
-            System.out.println("Dice Value is ==> " + diceValue);
+            int dicevalue = rand.nextInt(6) + 1; //it gives random numbers between 1 to 6
+            System.out.println("Dice Value is ==> " + dicevalue);
             int option = rand.nextInt(3); //for taking 3 random numbers for taking option
-
-            if (option == 0) {
+            if(position == winningPosition) {
+            	System.out.println("You Won The Game!!!!");
+            	break;
+            }
+            
+            switch(option) {
+            case 0:
             	System.out.println("No Play");
-            }else if (option == 1) {
+            	break;
+            case 1:
                 System.out.println("Ladder");
-                position += diceValue; //add dice value in position 
+                position += dicevalue; //add dice value in position 
                 System.out.println("Current Position is ==>" + position);
-            }else if (option == 2) {
-                System.out.println("Snake");
-                position -= diceValue;  //sub dice value from position
+                     if(position > 100) {
+                     	position = position - dicevalue;
+                     	System.out.println("Current Position is " +position);
+                     }
+                break;
+            case 2:
+            	System.out.println("Snake");
+                position -= dicevalue;  //sub dice value from position
                 System.out.println("Current Position is ==>" + position);
 
                 if (position < 0) {
                     position = 0;
                     System.out.println("Current Position is ==>" + position);
-                }
+                	}
             }
-        }
+		}
     }
 }
